527c205f117b090f2fb0732936962e3d2e3f2490
HanhPham <hanhphamhus@gmail.com>
2020-04-28 22:20:15 +0700

    npm init and npm install readline-sync, creat file index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+
+var readlineSync = require('readline-sync');
+
+function showMenu(){
+    console.log('1.Show all student');
+    console.log('2.Creat new student');
+    console.log('3.Save and Exit');
+  }
+  
+function main(){
+    showMenu();
+    var option = readlineSync.question('> ');
+    switch (option){
+        case '1':
+            showStudent();
+            break;
+        case '2':
+            
+    }
+
+  
+}
+main();
